582bbeaa26b10715de77ec9f39b72b810ebe6a0f
artvel <artan.v@gmx.ch>
2014-06-03 19:16:19 +0200

    ircwidget interface

---
+++ DroidsRocketColibri/src/ch/hsr/rocketcolibri/view/widget/IRCWidget.java
@@ -0,0 +1,66 @@
+package ch.hsr.rocketcolibri.view.widget;
+
+import java.util.Map;
+
+import android.view.View.OnTouchListener;
+import ch.hsr.rocketcolibri.view.custimizable.ViewElementConfig;
+import ch.hsr.rocketcolibri.widgetdirectory.UiOutputDataType;
+
+public interface IRCWidget {
+    /**This Method will be called on loading the whole Model at startup*/
+    void create(RCWidgetConfig rcWidgetConfig);
+    
+    /**This Method will be called on runtime in customize Mode by creating a new Widget*/
+    void create(ViewElementConfig vElementConfig);
+    
+    /**
+     * This Method sets the listener for the Control-Modus, this listener is not active
+     * in the Customize-Modus, the reference of it should be kept in the Class that implements
+     * this interface.
+     * */
+	void setControlModusListener(OnChannelChangeListener channelListener);
+	
+    /**
+     * This Method sets the listener for the Customize-Modus, this listener is not active
+     * in the Control-Modus, the reference of it should be kept in the Class that implements
+     * this interface.
+     * */
+	void setCustomizeModusListener(OnTouchListener customizeModusListener);
+	
+	/**
+	 * This Method is called from the DesktopViewManager on any change of the WidgetConfig
+	 * to keep the config synchronized with the Database.
+	 * @return
+	 */
+	RCWidgetConfig getWidgetConfig();
+	
+	/**
+	 * This Method is called on clicking on Edit Channel in the Customize-Modus Popup-Menu
+	 * to create the Edit-View with the Fields needed for this Widget
+	 */
+	Map<String, String> getProtocolMap();
+	
+	/**
+	 * This Method Overrides ProtocolMap and calls updateProtocolMap after.
+	 */
+	void setProtocolMap(Map<String, String> protocolMap);
+	
+	/** 
+	 * This Method is for implementing the update process after the ProtocolMap is changed.
+	 */
+	void updateProtocolMap();
+	
+	/**
+	 * RocketColibriService sends UiSink change notification with this methods
+	 * The Object class depends on the return value of getType
+	 * @param data
+	 */
+	void onNotifyUiOutputSink(Object data);
+	
+	/**
+	 * Override this function with the Ui Output data type the widget wants to receive with the onNotifyUiOutputSink method. 
+	 * @return type
+	 */
+	UiOutputDataType getType();
+
+}
